b6b47594f4c5e811b03c75ccfdbf6ad57f17394c
jsupratman13 <supratmanjoshua@gmail.com>
2025-02-19 11:30:01 +0900

    run components concurrent

--- b3m_hw_interface/src/b3m_hw_interface_node.cpp
+++ b3m_hw_interface/src/b3m_hw_interface_node.cpp
@@ -18,13 +18,73 @@
 #include <combined_robot_hw/combined_robot_hw.h>
 #include <controller_manager/controller_manager.h>
 #include <ros/ros.h>
+#include <vector>
+#include <future>
+
+class ParallelCombinedRobotHW : public combined_robot_hw::CombinedRobotHW
+{
+public:
+  bool init(ros::NodeHandle& nh, ros::NodeHandle& nh_private) override
+  {
+    root_nh_ = nh;
+    robot_hw_nh_ = nh_private;
+    std::vector<std::string> robots;
+    std::string param_name = "robot_hardware";
+    if (!robot_hw_nh_.getParam(param_name, robots))
+    {
+      ROS_ERROR_STREAM("Could not find '" << param_name << "' parameter (namespace: " << nh_private.getNamespace()
+                                          << ").");
+      return false;
+    }
+    std::vector<std::future<bool>> futures;
+    futures.reserve(robots.size());
+    for (const auto& robot : robots)
+    {
+      futures.push_back(std::async(std::launch::async, &ParallelCombinedRobotHW::loadRobotHW, this, robot));
+    }
+    for (auto& future : futures)
+    {
+      if (!future.get())
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+  void read(const ros::Time& time, const ros::Duration& period) override
+  {
+    std::vector<std::future<void>> futures;
+    futures.reserve(robot_hw_list_.size());
+    for (auto& robot_hw : robot_hw_list_)
+    {
+      futures.push_back(std::async(std::launch::async, &hardware_interface::RobotHW::read, robot_hw, time, period));
+    }
+    for (auto& future : futures)
+    {
+      future.get();
+    }
+  }
+  void write(const ros::Time& time, const ros::Duration& period) override
+  {
+    std::vector<std::future<void>> futures;
+    futures.reserve(robot_hw_list_.size());
+    for (auto& robot_hw : robot_hw_list_)
+    {
+      futures.push_back(std::async(std::launch::async, &hardware_interface::RobotHW::write, robot_hw, time, period));
+    }
+    for (auto& future : futures)
+    {
+      future.get();
+    }
+  }
+};
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "b3m_hw_interface_node");
   ros::NodeHandle nh, nh_private("~");
 
-  combined_robot_hw::CombinedRobotHW hw;
+  ParallelCombinedRobotHW hw;
   if (!hw.init(nh, nh_private))
   {
     ROS_FATAL_STREAM("Initializing hardware interfaces failed");
@@ -34,10 +94,8 @@ int main(int argc, char** argv)
   ROS_INFO_STREAM("Initializing hardware interfaces succeeded, starting hardware interface");
 
   controller_manager::ControllerManager cm(&hw, nh);
-  ros::AsyncSpinner spinner(1);
   ros::Rate rate(100);  // 100Hz update rate
 
-  spinner.start();
   while (ros::ok())
   {
     hw.read(ros::Time::now(), rate.expectedCycleTime());
@@ -45,6 +103,5 @@ int main(int argc, char** argv)
     hw.write(ros::Time::now(), rate.expectedCycleTime());
     rate.sleep();
   }
-  spinner.stop();
   return 0;
 }
